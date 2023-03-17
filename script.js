1889da0fc01d6a0db8a9b2db3ff047187dcbc58c
Dusky7 <victorsantos12dima@gmail.com>
2023-03-16 21:43:10 -0300

    finalizacao layout e add script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+
+    circulo.style.background = cor
+}
+
+function trocaImg(imagem) {
+    let imgiPhone = document.querySelector(".iphone")
+
+    imgiPhone.src = imagem
+}
